375174c02a0f022b8ab1f3ce62e4f7747dd79057
Amal P Sojan <amalpsojan@hotmail.com>
2022-03-06 14:47:51 +0530

    feat: added tests

---
+++ __tests__/scenes/TestScreen-test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+ import 'react-native';
+ import React from 'react';
+ 
+ // Note: test renderer must be required after react-native.
+ import renderer from 'react-test-renderer';
+ import {TestScreen} from '@appScenes';
+ 
+ it('TestScreen Scene renders correctly', () => {
+   renderer.create(<TestScreen />);
+ });
+ 
---
+++ e2e/Login/LoginView.e2e.js
@@ -0,0 +1,51 @@
+describe('MyDemoApp E2E Testing', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
+
+  it('should load Login screen', async () => {
+    await expect(element(by.id('login-view-scene'))).toBeVisible();
+  });
+
+  it('should login fail', async () => {
+    await element(by.id('login-view-email-input')).typeText(
+      'amalpsojan@example.com',
+    );
+    await element(by.id('login-view-password-input')).typeText('12345678');
+    await element(by.id('login-view-login-button')).tap();
+
+    // await waitFor(element(by.id('login-text-error'))).toBeVisible();
+  });
+
+  // it('should login fail', async () => {
+  //   await element(by.id('login-input-username')).typeText(
+  //     'amalpsojan@example.com',
+  //   );
+  //   await element(by.id('login-input-password')).typeText('12345678');
+  //   await element(by.id('login-button-login')).tap();
+
+  //   await waitFor(element(by.id('login-text-error'))).toBeVisible();
+  // });
+
+  // it('should login successfully', async () => {
+  //   await element(by.id('login-input-username')).clearText();
+  //   await element(by.id('login-input-username')).typeText('amalpp@gmail.com');
+
+  //   await element(by.id('login-input-password')).clearText();
+  //   await element(by.id('login-input-password')).typeText('123456');
+
+  //   await element(by.id('login-button-login')).tap();
+
+  //   await waitFor(element(by.id('profile-screen')))
+  //     .toBeVisible()
+  //     .withTimeout(10000);
+  // });
+
+  // it('should load Profile screen', async () => {
+  //   await expect(element(by.id('profile-screen'))).toBeVisible();
+  // });
+});
